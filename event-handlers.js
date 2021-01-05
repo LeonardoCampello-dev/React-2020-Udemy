2028a817f25d0c2c4e8c2cd052298ee78260f294
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-05 10:55:24 -0300

    Event handlers

---
+++ event-handlers.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <button
+            // onSubmit || onChange ..
+            onClick={(e) => {
+                window.alert("WOOOOW!");
+                console.info(e);
+            }}
+        >
+            Click here
+        </button>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
