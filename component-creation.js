66009384e0f891a83920652f15d578abec75c130
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-05 10:40:04 -0300

    Component creation

---
+++ component-creation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    // return React.createElement("h1", null, "Hello World!");
+    return (
+        <>
+            <h1 className="hello">Hello World!</h1>
+            <p>React is really cool</p>
+        </>
+    );
+}
+
+// ReactDOM.render(React.createElement(App), document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
