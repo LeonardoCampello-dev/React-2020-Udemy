25b46ce8fc5cf5ca4d01a0059bb872215513a79c
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-05 11:22:33 -0300

    Properties

---
+++ properties/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Welcome from "./Welcome";
+
+function App() {
+    return (
+        <>
+            {/* Generic component */}
+            <Welcome gender="male" name="Leonardo" />
+            <Welcome gender="female" name="Bianca" />
+            <Welcome />
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
