fb3afeefbb70f533176ea93477d0b407c17b866b
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-05 11:41:41 -0300

    State

---
+++ state.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    // By convention the function name always starts with "set" = setColor || setName ..
+    const [status, setStatus] = useState("Não");
+
+    return (
+        <>
+            <p>O botão foi clicado? {status}</p>
+
+            {/* Function to change state */}
+            <button
+                onClick={() => {
+                    setStatus("Sim");
+                }}
+            >
+                Clique
+      </button>
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
