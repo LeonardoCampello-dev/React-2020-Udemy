d9895ddbacca241da8af2ec8e72376b823f15d5e
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-06 10:00:45 -0300

    Hook useCallback()

---
+++ useCallback/index.js
@@ -0,0 +1,24 @@
+import React, { useCallback } from "react";
+import ReactDOM from "react-dom";
+
+import Button from "./Button";
+
+function App() {
+    const message = "Hello useCallback!";
+
+    const handleClick = useCallback((text) => {
+        window.alert(text);
+    }, []);
+
+    return (
+        <>
+            <Button
+                clicked={() => {
+                    handleClick(message);
+                }}
+            />
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
