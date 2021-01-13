264f290b806f9d11b8fab6aa6501efe7c5420f2e
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-13 16:45:05 -0300

    State in properties

---
+++ properties-and-state/state-in-properties/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import ColoredSquare from "./ColoredSquare";
+import ColorizerButton from "./ColorizerButton";
+
+function App() {
+    const [color, setColor] = useState("#8357e5");
+
+    return (
+        <div>
+            <ColoredSquare squareColor={color} />
+
+            <ColorizerButton
+                title="red"
+                clicked={() => {
+                    setColor("red");
+                }}
+            />
+
+            <ColorizerButton
+                title="green"
+                clicked={() => {
+                    setColor("green");
+                }}
+            />
+
+            <ColorizerButton
+                title="blue"
+                clicked={() => {
+                    setColor("blue");
+                }}
+            />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
