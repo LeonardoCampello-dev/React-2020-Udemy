185edb53d77606c6b4bfea16c96547403e3fb670
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-13 17:17:09 -0300

    Children special property

---
+++ properties-and-state/children-property/Icon.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const styles = {
+    width: "20px",
+    height: "20px",
+    borderRadius: "40%",
+    backgroundColor: "#533772"
+};
+
+function Icon() {
+    return <div style={styles} />;
+}
+
+export default Icon;
---
+++ properties-and-state/children-property/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Button from "./Button";
+import Icon from "./Icon";
+
+function App() {
+    return (
+        <div>
+            <Button>
+                <Icon />
+                <p>Children special property</p>
+            </Button>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
