a808066524027350baff8965c5da7e5ee5ead859
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-19 10:31:36 -0300

    Conditional rendering #2

---
+++ component-rendering/conditional-rendering-2/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    const [auth, setAuth] = useState(false);
+    let authButton;
+
+    if (auth) {
+        authButton = (
+            <button
+                onClick={() => {
+                    setAuth(false);
+                }}
+            >
+                Sair
+            </button>
+        );
+    } else {
+        authButton = (
+            <button
+                onClick={() => {
+                    setAuth(true);
+                }}
+            >
+                Entrar
+            </button>
+        );
+    }
+
+    return (
+        <>
+            <p>Você está logado? {auth ? "Sim" : "Não"}</p>
+            {authButton}
+        </>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
