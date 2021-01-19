39145f439058e8099a534cefb479c50f89b20ae3
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-19 10:03:30 -0300

    Conditional rendering #1

---
+++ component-rendering/conditional-rendering-1/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let posts = [
+    { id: "1", title: "As novidades do React 16" },
+    { id: "2", title: "ContextAPI e Redux: qual o melhor?" },
+    { id: "3", title: "As melhores APIs gratuitas para treinar" },
+    { id: "4", title: "Como integrar o Git no VS Code" },
+    { id: "5", title: "React 2021" }
+];
+
+function App() {
+    if (posts.length === 0) return <h1>Você tem 0 postagens!</h1>;
+
+    return <h1>Você tem {posts.length} postagens!</h1>;
+}
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
